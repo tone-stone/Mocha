1de872862a02b30c9ee4c9c4601522762ddc7b47
Tone <ing.antoniovilla@outlook.com>
2022-09-18 22:49:32 -0700

    first commit

---
+++ calculadora.js
@@ -0,0 +1,6 @@
+
+exports.addTest = value1 + value2;{
+
+var result = value1 + value2;
+return result;
+}
---
+++ prueba/calculadora-prueba.js
@@ -0,0 +1,33 @@
+
+var chai = require('chai');
+var assert = chai.assert;
+var should = chai.should();
+var expect = chai.expect;
+var calculator = require('../calculadora');
+
+describe('Testing assert function: ', function() {
+  describe('Check addTest Function', function(){
+    it('Check the returned value using : assert.equal(value, value): ', function() {
+       result = calculadora.addTest(1,1);
+       assert.equal(result, 2);
+    });
+  });
+})
+
+describe('Testing should function: ', function() {
+    describe('Check addTest Function', function(){
+      it('Check the returned value using : result.should.be.equal(value): ', function() {
+         result = calculadora.addTest(1,1);
+         result.should.be.equal(2);
+      })
+    })
+  })
+
+  describe('Testing expect function: ', function() {
+    describe('Check addTest Function', function(){
+      it('Check the returned value using : expect(result).to.be.a(value);: ', function() {
+         result = calculadora.addTest(1,1);
+          expect(result).to.equal(3);
+      })
+    })
+  })
